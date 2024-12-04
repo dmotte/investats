33813903c2ca651eeab67698c8b5cfc6e679d582
dmotte <37443982+dmotte@users.noreply.github.com>
2024-12-04 19:16:58 +0100

    Added some data to test aggregate_series

--- test/test_investats_aggr_cli.py
+++ test/test_investats_aggr_cli.py
@@ -8,7 +8,7 @@ import pytest
 from datetime import datetime as dt
 from datetime import timezone as tz
 
-from investats_aggr import pair_items_to_dict, load_data
+from investats_aggr import pair_items_to_dict, load_data, aggregate_series
 
 
 def test_pair_items_to_dict():
@@ -53,4 +53,73 @@ def test_save_data():
 
 
 def test_aggregate_series():
-    pass  # TODO
+    data_aaa = [
+        {'datetime': dt(2020, 1, 12).astimezone(),
+         'diff_days': 0, 'tot_days': 0,
+         'diff_src': 500, 'diff_dst': 5, 'latest_rate': 100,
+         'tot_src': 500, 'tot_dst': 5, 'avg_rate': 100,
+         'tot_dst_as_src': 500,
+         'chkpt_yield': 0, 'chkpt_apy': 0,
+         'global_yield': 0, 'global_apy': 0,
+         'latest_cgt': 0,
+         'chkpt_gain_src': 0, 'chkpt_gain_net_src': 0,
+         'tot_gain_src': 0, 'tot_gain_net_src': 0},
+        {'datetime': dt(2020, 2, 12).astimezone(),
+         'diff_days': 31, 'tot_days': 31,
+         'diff_src': 700, 'diff_dst': 10, 'latest_rate': 70,
+         'tot_src': 1200, 'tot_dst': 15, 'avg_rate': 80,
+         'tot_dst_as_src': 1050,
+         'chkpt_yield': -0.30000000000000004, 'chkpt_apy': -0.9849978210304741,
+         'global_yield': -0.125, 'global_apy': -0.7924170918049609,
+         'latest_cgt': 0.15,
+         'chkpt_gain_src': -150, 'chkpt_gain_net_src': -127.5,
+         'tot_gain_src': -150, 'tot_gain_net_src': -127.5},
+        {'datetime': dt(2020, 3, 12).astimezone(),
+         'diff_days': 29, 'tot_days': 60,
+         'diff_src': 250, 'diff_dst': 4.25, 'latest_rate': 200,
+         'tot_src': 1450, 'tot_dst': 19.25, 'avg_rate': 75.32467532467533,
+         'tot_dst_as_src': 3850,
+         'chkpt_yield': 1.8571428571428572, 'chkpt_apy': 547587.0028295065,
+         'global_yield': 1.6551724137931032, 'global_apy': 379.0996102191754,
+         'latest_cgt': 0.15,
+         'chkpt_gain_src': 2550, 'chkpt_gain_net_src': 2167.5,
+         'tot_gain_src': 2400, 'tot_gain_net_src': 2040},
+    ]
+    data_bbb = [
+        {'datetime': dt(2020, 1, 12).astimezone(),
+         'diff_days': 0, 'tot_days': 0,
+         'diff_src': 500, 'diff_dst': 10, 'latest_rate': 50,
+         'tot_src': 500, 'tot_dst': 10, 'avg_rate': 50,
+         'tot_dst_as_src': 500,
+         'chkpt_yield': 0, 'chkpt_apy': 0,
+         'global_yield': 0, 'global_apy': 0,
+         'latest_cgt': 0,
+         'chkpt_gain_src': 0, 'chkpt_gain_net_src': 0,
+         'tot_gain_src': 0, 'tot_gain_net_src': 0},
+        {'datetime': dt(2020, 2, 12).astimezone(),
+         'diff_days': 31, 'tot_days': 31,
+         'diff_src': 1400, 'diff_dst': 20, 'latest_rate': 70,
+         'tot_src': 1900, 'tot_dst': 30, 'avg_rate': 63.333333333333336,
+         'tot_dst_as_src': 2100,
+         'chkpt_yield': 0.3999999999999999, 'chkpt_apy': 51.546013724696195,
+         'global_yield': 0.10526315789473673, 'global_apy': 2.249177905018738,
+         'latest_cgt': 0.20,
+         'chkpt_gain_src': 200, 'chkpt_gain_net_src': 160,
+         'tot_gain_src': 200, 'tot_gain_net_src': 160},
+        {'datetime': dt(2020, 3, 12).astimezone(),
+         'diff_days': 29, 'tot_days': 60,
+         'diff_src': 250, 'diff_dst': 4.25, 'latest_rate': 200,
+         'tot_src': 2150, 'tot_dst': 34.25, 'avg_rate': 62.77372262773723,
+         'tot_dst_as_src': 6850,
+         'chkpt_yield': 1.8571428571428572, 'chkpt_apy': 547587.0028295065,
+         'global_yield': 2.186046511627907, 'global_apy': 1150.9943403101925,
+         'latest_cgt': 0.20,
+         'chkpt_gain_src': 4500, 'chkpt_gain_net_src': 3600,
+         'tot_gain_src': 4700, 'tot_gain_net_src': 3760},
+    ]
+
+    named_series = {'AAA': data_aaa, 'BBB': data_bbb}
+
+    # assert list(aggregate_series(named_series)) == [] # TODO
+
+    # TODO check errors with pytest.raises
