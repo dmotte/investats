c693d7b55b309910281af16642726ecedbe95ed2
dmotte <37443982+dmotte@users.noreply.github.com>
2026-01-09 10:00:06 +0100

    Added source link for is_aware function

--- investats/cli.py
+++ investats/cli.py
@@ -12,6 +12,7 @@ from typing import Any, TextIO
 import yaml
 
 
+# Src: https://github.com/dmotte/misc/tree/main/snippets
 def is_aware(d: dt) -> bool:
     '''
     Returns true if the datetime object `d` is timezone-aware, false otherwise.
